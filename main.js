6496d175c7cdfe29cb6999d0e5ad34fd41a508c4
AarushMohapatra <103845656+AarushMohapatra@users.noreply.github.com>
2022-06-02 20:35:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function spanelem() {
+    window.alert("span element");
+      document.getElementById("audio").play();
+    }
+    
+    function divelem() {
+     window.alert("div element"); document.getElementById("audio").pause();
+    }
+    
+    
+    
